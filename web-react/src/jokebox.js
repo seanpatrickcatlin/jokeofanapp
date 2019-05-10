4697e6260b248739f1e184f099161d38cd5f4196
Sean Catlin <catlin.sean@gmail.com>
2019-05-10 12:15:44 -0400

    cleanup and prettier

--- web-react/src/jokebox.js
+++ web-react/src/jokebox.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
 class JokeBox extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      joke: ''
+      joke: '',
     }
   }
 
@@ -18,21 +18,25 @@ class JokeBox extends React.Component {
       method: 'GET',
       headers: {
         Accept: 'application/json',
-        'User-agent': 'jokeofanapp web-rect (https://github.com/seanpatrickcatlin/jokeofanapp)'
-      }
-    }).then(response => response.json())
-    .then(({joke}) => {
-this.setState({
-  joke
-})    })
+        'User-agent':
+          'jokeofanapp web-rect (https://github.com/seanpatrickcatlin/jokeofanapp)',
+      },
+    })
+      .then(response => response.json())
+      .then(({ joke }) => {
+        this.setState({
+          joke,
+        })
+      })
   }
 
   render() {
     return (
       <div>
-        <h3>Papa Bless</h3>
+        <h2>Papa Bless</h2>
+        {/* h3h3 */}
         <h4>{this.state.joke}</h4>
-        <button onClick={this.fetchNewJoke}>pooe</button>
+        <button onClick={this.fetchNewJoke}>New Joke</button>
       </div>
     )
   }
